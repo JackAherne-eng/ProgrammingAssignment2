5cc0af4ca2571767bb292e5c97df4dd7a364dfa1
Jack Aherne <jack.aherne03@gmail.com>
2021-05-08 16:08:04 +0100

    Updated comments Utilities.java
    Addition of my comments to explain each method!

--- src/Utilities.java
+++ src/Utilities.java
@@ -1,9 +1,22 @@
+/**
+ * imports the ArrayList utility
+ */
 import java.util.ArrayList;
 
+/**
+ * The responsibility for the class was to store the reusable Utility methods in one area.
+ */
 public class Utilities {
 
+    /**
+     * numbersOnly will set something to only contain numbers
+     */
     private static String numbersOnly = "[0-9]+";
 
+    /**
+     * validEmail validates if the email contains
+     * and '@' and '.' symbols, else it returns false
+     */
     public static boolean validEmail(String email){
         if (email != null) {
             return (email.contains("@") && email.contains("."));
@@ -11,6 +24,10 @@ public class Utilities {
         return false;
     }
 
+    /**
+     * onlyContainsNumbers sets a string to only contain
+     * numbers.
+     */
     public static boolean onlyContainsNumbers(String text) {
         if (text != null) {
             return (text.matches(numbersOnly));
@@ -18,6 +35,11 @@ public class Utilities {
         return false;
     }
 
+    /**
+     * validPPS validates if the PPS number contains
+     * numbers within the first 7 characters and 2 letters
+     * at the end.
+     */
     public static boolean validPPS(String pps) {
         if(pps.length()==9){
             if(pps.substring(0, 7).matches("[0-9]+")){
@@ -29,18 +51,32 @@ public class Utilities {
         return false;
     }
 
+    /**
+     * Sets a string to have a max length of 10
+     */
     public static boolean max10Chars(String string){
         return (string.length()<=10) ? true : false;
     }
 
+    /**
+     * Sets a string to have a max length of 30
+     */
     public static boolean max30Chars(String string){
         return (string.length()<=30) ? true : false;
     }
 
+    /**
+     * validIntRange validates the int three is within the boundaries
+     * set by int one and int two.
+     */
     public static boolean validIntRange(int one, int two, int three){
         return three >= one && three <= two;
     }
 
+    /**
+     * validLecturerLevel validates the int lecLevel is within the boundaries
+     * of number 1 and 3. It uses the utility validIntRange.
+     */
     public static boolean validLecturerLevel(int lecLevel){
         if(validIntRange(1,3,lecLevel)){
             return true;
@@ -48,6 +84,10 @@ public class Utilities {
         return false;
     }
 
+    /**
+     * validMangerLevel validates the int mangLevel is within the boundaries
+     * of number 1 and 7. It uses the utility validIntRange.
+     */
     public static boolean validMangerLevel(int mangLevel){
         if(validIntRange(1,7,mangLevel)){
             return true;
@@ -55,6 +95,10 @@ public class Utilities {
         return false;
     }
 
+    /**
+     * validAdminLevel validates the int adminLevel is within the boundaries
+     * of number 1 and 4. It uses the utility validIntRange.
+     */
     public static boolean validAdminLevel(int adminLevel){
         if(validIntRange(1,4,adminLevel)){
             return true;
@@ -62,19 +106,32 @@ public class Utilities {
         return false;
     }
 
+    /**
+     * validIntNonNegative validates that nonNeg is equal or greater than 0.
+     */
     public static boolean validIntNonNegative(int nonNeg){
         return nonNeg >= 0;
     }
 
+    /**
+     * validDoubleNonNegative validates that doubNonNeg is equal or greater than 0.
+     */
     public static boolean validDoubleNonNegative(int doubNonNeg){
         return doubNonNeg >= 0;
     }
 
-
+    /**
+     * validIndex validates that the index is equal or greater than 0
+     * and less that the size of the ArrayList.
+     */
     public static boolean validIndex(int index, ArrayList arrayList){
         return index >= 0 && index < arrayList.size();
     }
 
+    /**
+     * grade sets the int is within the boundaries
+     * of number 1 and 7. It uses the utility validIntRange.
+     */
     public static boolean Grade(int grade){
         if(validIntRange(1,7,grade)){
             return true;
@@ -82,6 +139,11 @@ public class Utilities {
         return false;
     }
 
+    /**
+     * getSalaryForLecturerLevel sets the int is within the boundaries
+     * of number 1 and 3. It uses the utility validIntRange and multiplies
+     * salary by 1000.
+     */
     public static double getSalaryForLecturerLevel(int salary){
         if(validIntRange(1,3,salary)){
             return salary*1000;
@@ -89,6 +151,11 @@ public class Utilities {
         return 0;
     }
 
+    /**
+     * getSalaryForAdminGrade sets the int is within the boundaries
+     * of number 1 and 7. It uses the utility validIntRange and multiplies
+     * salaryGrade by 700.
+     */
     public static double getSalaryForAdminGrade(int salaryGrade){
         if(validIntRange(1,7,salaryGrade)){
             return salaryGrade*700;
